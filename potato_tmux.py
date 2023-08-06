275073cd70dffbc45ce725a6267af204f92fe779
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-08-06 15:39:25 -0400

    ok

--- potato_tmux.py
+++ potato_tmux.py
@@ -3,5 +3,5 @@ import tmuxer
 
 SESSIONS_FILENAME = "potato_sessions.txt"
 
-sessions_file = os.path.join(os.path.dirname(__file__), SESSIONS_FILENAME)
+sessions_file = os.path.join(os.path.dirname(__file__), "tasks", SESSIONS_FILENAME)
 tmuxer.import_and_run(sessions_file)
