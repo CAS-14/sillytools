165c5aa88efcb8ae439a2119df25ae10ec467968
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-08-06 15:28:01 -0400

    fix

--- tasks/potato_init.sh
+++ tasks/potato_init.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+if [[ -z "$TOOLSDIR" ]]; then
+    export TOOLSDIR="/home/cas/code/my-silly-tools"
+fi
+
 cp "$TOOLSDIR/dotfiles/potato/.bash_aliases" "$HOME"
 bash "$TOOLSDIR/tasks/potato_tmux.sh"
