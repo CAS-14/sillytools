ac278ee83d10622ddec3f1a359084101a01f4840
CAS <78571139+cas-14@users.noreply.github.com>
2023-12-03 15:40:47 -0500

    os.walk

--- reference.py
+++ reference.py
@@ -8,3 +8,9 @@ os.path.dirname(__file__)
 # earlier than 3.9
 os.path.dirname(os.path.abspath(__file__))
 
+# os.walk usage
+for root, dirs, files in os.walk("."):
+    for name in files:
+        print(f"file: {os.path.join(root, name)}")
+    for name in dirs:
+        print(f"dir: {os.path.join(root, name)}")
